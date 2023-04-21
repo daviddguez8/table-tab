7c42b160f93207c8dc0cfaafd029efc00359a76b
Juan <jtn1701@hotmail.com>
2023-04-20 20:13:07 -0600

    More Tables.

--- src/data/tables.js
+++ src/data/tables.js
@@ -30,5 +30,29 @@ export const TABLES = {
         maxPeople: 6,
         waiter: '',
         tab: []
+    },
+    'Table 5': {
+        available: true,
+        needsHelp: false,
+        people: 0,
+        maxPeople: 10,
+        waiter: '',
+        tab: []
+    },
+    'Table 6': {
+        available: true,
+        needsHelp: false,
+        people: 0,
+        maxPeople: 12,
+        waiter: '',
+        tab: []
+    },
+    'Table 7': {
+        available: true,
+        needsHelp: false,
+        people: 0,
+        maxPeople: 8,
+        waiter: '',
+        tab: []
     }
 };
