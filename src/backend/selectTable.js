e635c49cdccdc2a732f92fabe58794b5faf86c4e
agamez8 <agamez8@miners.utep.edu>
2023-05-03 12:19:21 -0600

    customer view

---
+++ src/backend/selectTable.js
@@ -0,0 +1,23 @@
+import { TABLES } from "../data/tables";
+
+/**
+ * getNextAvailable - finds the next available table for a given number of people
+ *
+ * @param {number} people - number of people in the group
+ * @returns {string|null} - table number (e.g. "Table 1") or null if no table is available
+ */
+export const getNextAvailable = (people) => {
+    if (people <= 0) {
+        return null; 
+    }
+
+    let availableTable = null;
+
+    for (const table in TABLES) {
+      if (TABLES[table].available && TABLES[table].maxPeople >= people) {
+        availableTable = table;
+        break;
+      }
+    }
+    return availableTable; // no available tables found
+};
--- src/components/CustomerView/CustomerView.js
+++ src/components/CustomerView/CustomerView.js
@@ -1,8 +1,110 @@
+import './customerView.css';
+import { Container } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import { Button } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
+
+import { useState } from 'react';
+import { MENU } from '../../data/menu';
+
+
 function CustomerView() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [addedItems, setAddedItems] = useState([]);
+  const [showAddedItems, setShowAddedItems] = useState(false);
+
+  const handleViewMenu = () => {
+    setShowMenu(true);
+  };
+  const handleCloseMenu = () => {
+    setShowMenu(false);
+  };
+
+  const handlePay = () => {
+    setShowAddedItems(true);
+  };
+  const handleCloseAddedItems = () => {
+    setShowAddedItems(false);
+  };
+  const handleAddItem = (item) => {
+    setAddedItems([...addedItems, item]);
+  };
+
   return (
-    <div className="App">
-        This will be the customer view
-    </div>
+    <Container className='customer-main-container'>
+      <Row className='fixed-top customer-topbar'>
+        <h1>Customer View</h1>
+      </Row>
+
+      {/* Buttons Container */}
+      <Row className='customer-content-container mb-3'>
+        <Row className='mt-5 justify-content-center'>
+          <Col className='text-center'>
+            <Button className='w-100' variant='primary' onClick={handleViewMenu}>View Menu</Button>
+          </Col>
+        </Row>
+        <Row className='mt-3 justify-content-center'>
+          <Col className='text-center'>
+            <Button className='w-100' variant='primary' onClick={handlePay}>Pay</Button>
+          </Col>
+        </Row>
+        <Row className='mt-3 justify-content-center'>
+          <Col className='text-center'>
+            <Button className='w-100' variant='primary'>Call Waiter</Button>
+          </Col>
+        </Row>
+      </Row>
+
+      {/* Menu Modal */}
+      <Modal show={showMenu} onHide={handleCloseMenu}>
+        <Modal.Header closeButton>
+          <h2>Menu</h2>
+        </Modal.Header>
+        <Modal.Body>
+          {/* Show menu */}
+          <ul>
+            {MENU.map((food) => (
+              <li key={food.id}>
+                <h3>{food.name}</h3>
+                <p>Price: ${food.price}</p>
+              </li>
+            ))}
+          </ul>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseMenu}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* Tab Modal */}
+      <Modal show={showAddedItems} onHide={handleCloseAddedItems}>
+        <Modal.Header closeButton>
+          <h2>Tab</h2>
+        </Modal.Header>
+        <Modal.Body>
+          {/* Shows tab */}
+          <ul>
+            {addedItems.map((item, index) => (
+              <li key={index}>
+                <h3>{item.name}</h3>
+                <p>Price: ${item.price}</p>
+              </li>
+            ))}
+          </ul>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={() => {}}>
+            Pay All
+          </Button>
+          <Button variant="primary" onClick={() => {}}>
+            Split Bill
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+    </Container>
   );
 }
 
