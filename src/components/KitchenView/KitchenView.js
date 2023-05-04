fd81f0f28dcf75b8207e8b55069d404bea17dcd7
Juan <jtn1701@hotmail.com>
2023-05-04 16:04:15 -0600

    Need to resolve how to delete an item.

--- src/components/KitchenView/KitchenView.js
+++ src/components/KitchenView/KitchenView.js
@@ -1,17 +1,16 @@
 import {Container, Form, Row, Button, Table} from 'react-bootstrap';
 import './KitchenView.css';
-import { useEffect, useState } from 'react';
-import { KITCHEN_STATUSES } from "../../data/statuses";
+import {useEffect, useState} from 'react';
+import {KITCHEN_STATUSES} from "../../data/statuses";
 import {fetchToTables, pushTableToFirebase} from "../../backend/firestore";
 
 
 // This is the kitchen view.
 function KitchenView() {
     const [TABLES, setTables] = useState({});
-    const [ item, setItems] = useState([]);
 
     useEffect(() => {
-        fetchToTables(setTables)
+        fetchToTables(setTables).then(r => console.log(r))
     }, []);
 
     const handleStatusChange = async (tableId, itemIdx, newStatus) => {
@@ -23,10 +22,13 @@ function KitchenView() {
         await fetchToTables(setTables);
     }
 
-    const handleItemReady = (index, item) => {
-        const updatedItems = [item];
-        updatedItems.splice(index, 1);
-        setItems(updatedItems);
+    const handleItemReady = (tableId, index, item) => {
+        console.log("Ready", tableId, index, item)
+        // setTables((prev) => {
+        //     const updatedTable = { ...prev[tableId]};
+        //     updatedTable.tab.splice(index, 1);
+        //     return { prev, [tableId]: updatedTable };
+        // })};
     }
 
     return (
@@ -39,62 +41,62 @@ function KitchenView() {
                 <h2>Kitchen View</h2>
             </Row>
 
-                <Row className="table-container">
-                    <Container className="table-info-container mb-3">
-                        <h3 className="mb-3">Items to Cook</h3>
-                    </Container>
+            <Row className="table-container">
+                <Container className="table-info-container mb-3">
+                    <h3 className="mb-3">Items to Cook</h3>
+                </Container>
 
-                    <Table striped bordered hover>
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Item</th>
-                                <th>Quantity</th>
-                                <th>Status</th>
-                                <th>Call Waiter?</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {/*TABLES[selectedTable]*/}
-                        {Object.keys(TABLES).map((tableId) => {
-                            const table = TABLES[tableId];
-                            // console.log(TABLES[tableId])
-                            return table.tab.map((item, index) => {
-                                console.log("Grillo", table)
-                                return (
-                                    <tr key={`${tableId}-${index}`}>
-                                        <td>{tableId}</td>
-                                        <td>{item.name}</td>
-                                        <td>{item.quantity}</td>
-                                        <td>
-                                            <Form.Select required aria-label="Default select example"
-                                                         defaultValue={item.status}
-                                                         onChange={(e) => {
-                                                             handleStatusChange(tableId, index, e.target.value)
-                                                         }}>
-                                                {Object.values(KITCHEN_STATUSES).map((status) => {
-                                                    return (
-                                                        <option key={status}
-                                                                selected={item.status === status}>{status}</option>
-                                                    )
-                                                })}
-                                            </Form.Select>
-                                        </td>
-                                        <td>
-                                            {item.status === 'Cooked' ? (
-                                                <Button className='w-100' variant='primary'
-                                                        onClick={() => handleItemReady(index, item)}>
-                                                    Item is Ready
-                                                </Button>
-                                            ) : <span>Cooking...</span>}
-                                        </td>
-                                    </tr>
-                                )
-                            })
-                        })}
-                        </tbody>
-                    </Table>
-                </Row>
+                <Table striped bordered hover>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Item</th>
+                        <th>Quantity</th>
+                        <th>Status</th>
+                        <th>Call Waiter?</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    {Object.keys(TABLES).map((tableId) => {
+                        const table = TABLES[tableId];
+                        // console.log(TABLES[tableId])
+                        return table.tab.map((item, index) => {
+                            console.log("Grillo", table)
+                            return (
+                                <tr key={`${tableId}-${index}`}>
+                                    <td>{tableId}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.quantity}</td>
+                                    <td>
+                                        <Form.Select required aria-label="Default select example"
+                                                     defaultValue={item.status}
+                                                     onChange={(e) => {
+                                                         handleStatusChange(tableId, index, e.target.value)
+                                                     }}>
+                                            {Object.values(KITCHEN_STATUSES).map((status) => {
+                                                return (
+                                                    <option key={status}
+                                                            selected={item.status === status}>{status}</option>
+                                                )
+                                            })}
+                                        </Form.Select>
+                                    </td>
+                                    <td>
+                                        {item.status === 'Cooked' ? (
+                                            <Button className='w-100' variant='primary'
+                                                    onClick={() => handleItemReady(tableId, index, item)}>
+                                                Item is Ready
+                                            </Button>
+                                        ) : <span>Cooking...</span>}
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    })}
+                    </tbody>
+                </Table>
+            </Row>
         </Container>
     );
 }
